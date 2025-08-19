2a75a60b93c1f26fb72ed6091108c7c043bf2fc7
Manuel <manuelmaddaluno5@gmail.com>
2025-08-19 16:13:26 +0200

    [hw_manager] add target selection logic based on the request device in case of multiple targets

--- hw/xilinx/scripts/utils/open_hw_manager.tcl
+++ hw/xilinx/scripts/utils/open_hw_manager.tcl
@@ -8,16 +8,27 @@ if {[catch {connect_hw_server -url $url} 0]} {
     puts stderr "WARNING: Another connection is already up, proceeding using the existing connection instead"
 }
 set target_path $::env(XILINX_HW_SERVER_HOST):$::env(XILINX_HW_SERVER_PORT)/$::env(XILINX_HW_SERVER_FPGA_PATH)
-set hw_target [lindex [get_hw_target $target_path] 0]
-# Device at index zero
-open_hw_target $hw_target
-# Let's keep JTAG frequency to 5000000 so that it works with all supported SoC frequencies
-set_property PARAM.FREQUENCY 5000000 [get_hw_targets $hw_target]
 
-# Set hw_device
-set hw_device [get_hw_devices $::env(XILINX_HW_DEVICE)]
-# Select hw device
-current_hw_device $hw_device
+# Search the target with the right device
+foreach target [get_hw_target $target_path] {
+    open_hw_target $target
+    # Let's keep JTAG frequency to 5000000 so that it works with all supported SoC frequencies
+    set_property PARAM.FREQUENCY 5000000 [get_hw_targets $target]
+
+    # Check if the actual target has the right device
+    set hw_devices [get_hw_devices]
+    set hw_device_id [lsearch -exact $hw_devices $::env(XILINX_HW_DEVICE)]
+
+    # Set the hw device
+    if {$hw_device_id >= 0} {
+        set hw_target $target
+        set hw_device [lindex $hw_devices $hw_device_id]
+        break
+    }
+
+    # Close the hw target if not have the right device
+    close_hw_target
+}
 
 # Set bitstream path
 set_property PROGRAM.FILE $::env(XILINX_BITSTREAM) $hw_device
@@ -39,4 +50,4 @@ current_hw_device $hw_device
 ###################
 # Get debug cores #
 ###################
-refresh_hw_device [lindex $hw_device 0]
+refresh_hw_device $hw_device
