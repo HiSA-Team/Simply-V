ac953450207dfc115248e551637f1852ad9c60f3
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2025-10-11 16:47:55 +0200

    [refactor] XDMA readback script

--- hw/xilinx/scripts/utils/readback_xdma.sh
+++ hw/xilinx/scripts/utils/readback_xdma.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 # Author: Vincenzo Maisto <vincenzo.maisto2@unina.it>
 # Description: Bash script to read back from AXI using busybox devmem
-# Usage: ./axi_readback.sh <base_address> <num_bytes>
+# Arguments:
+#   $1: PCIe BAR
+#   $2: Offset in SoC
+#   $2: Number of bytes to read
 
 # --------------------------
 # Usage and argument parsing
 # --------------------------
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <PCIE_BAR> <base_address> <num_bytes>"
+    echo "Usage: $0 <PCIE_BAR> <offset> <num_bytes>"
     echo "Example: $0 0x92000000 0x40000 64"
-    BASE_ADDR=0x0000
+    OFFSET=0x0000
     NUM_BYTES=16
 else
     PCIE_BAR=$1
-    BASE_ADDR=$2
+    OFFSET=$2
     NUM_BYTES=$3
 fi
 
@@ -25,7 +28,7 @@ READ_SIZE_BITS=$(($BURST_SIZE * 8)) # bits per read
 NUM_BURSTS=$((NUM_BYTES / BURST_SIZE))
 
 echo "PCIe BAR: $PCIE_BAR"
-echo "Base address: $BASE_ADDR"
+echo "Base address: $OFFSET"
 echo "Number of bytes to read: $NUM_BYTES"
 echo "Reading $NUM_BURSTS bursts of $BURST_SIZE bytes each"
 echo "-------------------------------------------"
@@ -33,7 +36,7 @@ echo "-------------------------------------------"
 # --------------------------
 # Convert hex to decimal for arithmetic
 # --------------------------
-base_dec=$(($BASE_ADDR + $PCIE_BAR))
+base_dec=$(($OFFSET + $PCIE_BAR))
 
 # --------------------------
 # Read loop
