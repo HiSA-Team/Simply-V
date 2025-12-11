c35c9434cd878be149ce8ba6e3ccb3ddc65aed75
DOTTORM5 <manuelmaddaluno5@gmail.com>
2025-12-11 17:57:04 +0100

    [hbm/sw] update HBM SW test

--- sw/SoC/examples/xlnx_hbm/src/main.c
+++ sw/SoC/examples/xlnx_hbm/src/main.c
@@ -7,16 +7,29 @@
 #include "uninasoc.h"
 #include <stdint.h>
 
+#define HBM_BASE_ADDRESS 0x60000
+
 int main()
 {
 
-  // Initialize HAL
-  uninasoc_init();
+    // Initialize HAL
+    uninasoc_init();
 
+    // Pointing to the HBM
+    uint32_t * hbm = (uint32_t *) HBM_BASE_ADDRESS;
 
+    printf("[HBM] Executing HBM test\n\r");
 
-  // Return to caller
-  return 0;
+    uint32_t store_value = 0xDEADBEEF;
+
+    printf("[HBM] Storing %d\n\r", store_value);
+    *hbm = 0xDEADBEEF;
+
+    uint32_t load_value = *hbm;
+    printf("[HBM] Loading %d\n\n", load_value);
+
+    // Return to caller
+    return 0;
 
 }
 
