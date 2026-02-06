c73290ca31ed7e6d7a49cff2ce6fa67b8b0a3ae5
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2026-02-06 17:54:56 +0100

    [fix] PLIC IP_NAME var in fetch sources

--- hw/units/custom_rv_plic/fetch_sources.sh
+++ hw/units/custom_rv_plic/fetch_sources.sh
@@ -8,6 +8,7 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
+IP_NAME=$( basename $(dirname $( realpath ${BASH_SOURCE[0]} ) ))
 
 # Directories
 RTL_DIR=$(pwd)/rtl
