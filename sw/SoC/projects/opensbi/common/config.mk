67b11ce8d3f6e7665f980048dd4e0e3fbeab7ed6
giuseppe <capassog97@gmail.com>
2026-02-27 19:13:59 +0100

    repo: removed opensbi/common/config.mk

--- sw/SoC/projects/opensbi/common/config.mk
+++
@@ -1,6 +0,0 @@
-# This file should be modified by configuration script to provide the correct FW_TEXT_START variable.
-# This is needed since OpenSBI firmware linkerscript places the firmware starting from this variable.
-#
-# Author: Giuseppe Capasso <giuseppe.capasso17@studenti.unina.it>
-
-OPENSBI_FW_TEXT_START ?= 0x1000000
